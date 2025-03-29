6c67fcd36831749fca7785b9aa21400b2839d43b
thaArcadeGuy <gilbertsilver64@gmail.com>
2025-03-29 15:42:20 +0300

    first cycle

---
+++ test/test.js
@@ -0,0 +1,8 @@
+import { assert } from "chai";
+import {describe, it} from "node:test";
+
+describe("Multiplication function", () => {
+  it("should return 1 when you multiply 1 by 1", () => {
+    assert.strictEqual(multiply(1, 1), 1);
+  });
+});
